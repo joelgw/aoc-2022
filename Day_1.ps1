9a5ccb37bc020a835e20dce6cc812341a4d00031
joelgw <38991531+joelgw@users.noreply.github.com>
2022-12-01 15:36:02 -0600

    Day_1 Powershell

---
+++ Day_1.ps1
@@ -0,0 +1,23 @@
+﻿$calories = Get-Content C:\Users\\Documents\calories.txt
+
+$total_cals = @()
+$running_sum = 0
+
+For($i=0; $i -le $calories.Count; $i++) {
+    if([string]::IsNullOrWhiteSpace($calories[$i])) { 
+        $total_cals += $running_sum 
+        $running_sum = 0
+    } 
+    else { 
+        $running_sum += $calories[$i]
+    }
+}
+
+# Part 1 Answer
+($total_cals | measure -Maximum).Maximum
+
+#Part 2
+
+$sorted_cals = ($total_cals | Sort-Object -Descending) 
+
+$sorted_cals[0] + $sorted_cals[1] + $sorted_cals[2]
